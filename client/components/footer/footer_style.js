2ab8789f23b48d0b1277fdb7860e9525e40beaff
IrinaMalysheva <irina.malysheva@onlyoffice.com>
2021-06-30 10:29:52 +0300

    Add personal's footer.

--- client/components/footer/footer_style.js
+++ client/components/footer/footer_style.js
@@ -1,7 +1,107 @@
 import styled from "styled-components";
+import color_social_icons from '../../public/color_social_icons.svg';
 
 const StyleFooter = styled.footer`
+    background: #f9f9f9;
+    font-size: 14px;
+    line-height: 160%;
+    position: relative;
     width: 100%;
+    
+    a {
+        color: #444;
+        text-decoration: underline;
+    }
+
+    .ListSocLink {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: flex-end;
+        margin: 0;
+        padding: 0;
+
+        li {
+            list-style-type: none;
+            display: inline-block;
+            width: 24px;
+            height: 24px;
+            margin: 0;
+            padding-right: 16px;
+
+            a {
+                cursor: pointer;
+                display: block;
+                height: 24px;
+                width: 24px;
+            }
+
+            label {
+                background-repeat: no-repeat;
+                background-image: url(${color_social_icons});
+                cursor: pointer;
+                filter: grayscale(1);
+                height: 24px;
+                width: 24px;
+
+                &:hover {
+                    filter: grayscale(0)
+                }
+
+                &.social_grey_fb {
+                    background-position-x: 2px
+                }
+
+                &.social_grey_twi {
+                    background-position-x: -38px
+                }
+
+                &.social_grey_in {
+                    background-position-x: -78px
+                }
+
+                &.social_grey_tube {
+                    background-position-x: -118px
+                }
+
+                &.social_grey_blog {
+                    background-position-x: -198px
+                }
+
+                &.social_grey_medium {
+                    background-position-x: -238px
+                }
+
+                &.social_grey_instagram {
+                    background-position-x: -278px
+                }
+
+                &.social_grey_github {
+                    background-position-x: -318px
+                }
+
+                &.social_grey_fosstodon {
+                    background-position-x: -395px
+                }
+            }
+        }
+    }
+
+    .partial-personal-footer {
+        display: flex;
+        justify-content: space-between;
+        padding-top: 32px;
+    }
+
+    .personal-footer-links span {
+        margin-right: 24px;
+    }
+    
+    .personal-footer_w {
+        background: #f9f9f9;
+        margin: 0 auto;
+        width: 1120px;
+        padding: 0 0 40px;
+    }
 `;
 
 export default StyleFooter;
--- client/components/footer/index.js
+++ client/components/footer/index.js
@@ -2,26 +2,82 @@ import React from "react";
 import StyleFooter from "./footer_style";
 import Link from "next/link";
 import Image from "next/image";
+import color_social_icons from '../../public/color_social_icons.svg';
+import fileloader from "file-loader";
+import style from "../../styles/St.module.css";
 
 class Footer extends React.Component {
     render() {
         return(
-            <StyleFooter>
-                <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                <div>
-
-                    <p></p>
-                    <Link href="/1">Terms of Service</Link>
-                    <Link href="/2">Corporate Use</Link>
-                    <span>© Ascensio System SIA 2021. All rights reserved</span>
+            <StyleFooter img={color_social_icons}>
+                <div className="personal-footer_w">
+                    <ul className={style.ListSocLink}>
+                        <li>
+                            <label className={style.social_grey_fb} title="Facebook" style={{ backgroundImage: `url(${color_social_icons})` }}>
+                                <Link className="faceBook" href="https://www.facebook.com/pages/OnlyOffice/833032526736775" title="Facebook"> </Link>
+                            </label>
+                        </li>
+                        <li>
+                            <label title="Twitter" className={style.social_grey_twi}>
+                                <Link className="twitter" href="https://twitter.com/ONLY_OFFICE" rel="nofollow"> </Link>
+                            </label>
+                        </li>
+                        <li>
+                            <label title="LinkedIn" className={style.social_grey_in}>
+                                <Link className="linkedin" href="https://www.linkedin.com/groups/6726380/" rel="nofollow"> </Link>
+                            </label>
+                        </li>
+                        <li>
+                            <label title="YouTube" className={style.social_grey_tube}>
+                                <Link className="youtube" href="https://www.youtube.com/user/onlyofficeTV" rel="nofollow"> </Link>
+                            </label>
+                        </li>
+                        <li>
+                            <label title="Blog" className={style.social_grey_blog}>
+                                <Link className="blog" href="https://www.onlyoffice.com/blog"> </Link>
+                            </label>
+                        </li>
+                        <li>
+                            <label title="Medium" className={style.social_grey_medium}>
+                                <Link className="medium" href="https://medium.com/onlyoffice"> </Link>
+                            </label>
+                        </li>
+                        <li>
+                            <label title="Instagram" className={style.social_grey_instagram}>
+                                <Link className="instagram" href="https://www.instagram.com/the_onlyoffice/"> </Link>
+                            </label>
+                        </li>
+                        <li>
+                            <label title="GitHub" className={style.social_grey_github}>
+                                <Link className="github" href="https://github.com/ONLYOFFICE/"> </Link>
+                            </label>
+                        </li>
+                        <li>
+                            <label title="Fosstodon" className={style.social_grey_fosstodon}>
+                                <Link className="fosstodon" href="https://fosstodon.org/@ONLYOFFICE"> </Link>
+                            </label>
+                        </li>
+                    </ul>
+                    {/* <Image src="/color_social_icons.svg" alt="Vercel Logo" width={72} height={16} />
+                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
+                    <div className="partial-personal-footer">
+                        <div className="personal-footer-links">
+                            <span>
+                                <Link href="/Terms.aspx">Terms of Service</Link>
+                            </span>
+                            <span>
+                                <Link href="https://www.onlyoffice.com">Corporate Use</Link>
+                            </span>
+                        </div>
+                        <span>© Ascensio System SIA 2021. All rights reserved</span>
+                    </div>
                 </div>
             </StyleFooter>
         );
